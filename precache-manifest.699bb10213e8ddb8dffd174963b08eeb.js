270dacd599e0e84d1fde6964e7514a47361fe70c
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-05-20 16:32:50 -0400

    Updates

---
+++ precache-manifest.699bb10213e8ddb8dffd174963b08eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de4c2311886f38cd9ff7ade14d38e46",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "9788b8af1ab8cd0c9fc3",
+    "url": "/dad-jokes/static/css/main.faabd244.chunk.css"
+  },
+  {
+    "revision": "9714d3f59c8761c59bd9",
+    "url": "/dad-jokes/static/js/2.e76f468b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes/static/js/2.e76f468b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9788b8af1ab8cd0c9fc3",
+    "url": "/dad-jokes/static/js/main.13585112.chunk.js"
+  },
+  {
+    "revision": "c56de877541203b8d3dd",
+    "url": "/dad-jokes/static/js/runtime-main.a9ae9712.js"
+  }
+]);
